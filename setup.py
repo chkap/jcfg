9ef748b5d45ad842cd0a848668c25afedea742ed
kai <chkap>
2019-01-19 22:58:10 +0800

    new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('./README.md') as rfile:
 
 setuptools.setup(
     name='jcfg',
-    version='0.4.0',
+    version='0.4.1',
     description='A json-based configuration helper lib for python',
     long_description=long_desc,
     long_description_content_type='text/markdown',
